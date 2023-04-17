79df487e78da2ca8cd2f0404444495aed6768e02
zhao_jianping <zhao_jianping@cdv.com>
2023-04-17 22:55:49 +0800

    feat: 图片的TS声明文件

---
+++ types/image.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.gif";
+declare module "*.tiff";
+declare module "*.bmp";
+declare module "*.webp";
